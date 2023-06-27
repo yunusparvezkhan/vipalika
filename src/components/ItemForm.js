9683328d54e44fdb868508e06c7a01be05065f66
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-27 23:57:29 +0530

    feat: add another input [cost] in ItemForm
    this commit adds another input field named [cost] in
    ItemForm component for handling item's cost inputs.
    And also this commits adds all the logics to access and
    update the cost redux state.

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { name, addName } from '../store';
+import { name, addName, addCost } from '../store';
 
 const ItemForm = () => {
 
     const dispatch = useDispatch();
 
-    const name = useSelector((state) => {
-        return state.form.name;
+    const { name, cost } = useSelector((state) => {
+        return state.form;
     })
 
     const handleNameChange = (event) => {
         dispatch(addName(event.target.value));
     }
 
+    const handleCostChange = (event) => {
+        dispatch(addCost(event.target.value));
+    }
+
     return (
         <div className='item-form panel'>
             <h3 className='subtitle is-3'>Add Item</h3>
             <div className='field-group'>
+
                 <div className='field'>
-                    <label className='label'>Name</label>
+                    <label className='label'> Name </label>
                     <input className='input is-expanded' value={name} onChange={handleNameChange} />
+                </div>
+
+                <div className='field'>
+                    <label className='label'>Cost ₹ </label>
+                    <input type='number' className='input is-expanded' value={cost} onChange={handleCostChange} />
                 </div>
 
             </div>
