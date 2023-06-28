45c2b52ce56b70523c07f94d69b1382ccd85f20d
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-28 13:53:22 +0530

    feat: add submit mechanism in ItemsForm
    this commit adds a submit button in ItemsForm and all the neccassary
    codes to update the [items] piece of state in the redux store.

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addName, addCost } from '../store';
+import { addName, addCost, addItem } from '../store';
 
 const ItemForm = () => {
 
@@ -18,23 +18,32 @@ const ItemForm = () => {
         dispatch(addCost(parseInt(event.target.value) || 0));
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(addItem({ name, cost }))
+    }
+
     return (
         <div className='item-form panel'>
             <h3 className='subtitle is-3'>Add Item</h3>
-            <div className='field-group'>
+            <form onSubmit={handleSubmit}>
+                <div className='field-group'>
+                    <div className='field'>
+                        <label className='label'> Name </label>
+                        <input className='input is-expanded' value={name} onChange={handleNameChange} />
+                    </div>
 
-                <div className='field'>
-                    <label className='label'> Name </label>
-                    <input className='input is-expanded' value={name} onChange={handleNameChange} />
+                    <div className='field'>
+                        <label className='label'>Cost ₹ </label>
+                        <input type='number' className='input is-expanded' value={cost || ''} onChange={handleCostChange} />
+                    </div>
+
+                    <div className='field'>
+                        <button className='button is-link'  >Submit</button>
+                    </div>
                 </div>
-
-                <div className='field'>
-                    <label className='label'>Cost ₹ </label>
-                    <input type='number' className='input is-expanded' value={cost || ''} onChange={handleCostChange} />
-                </div>
-
-            </div>
-        </div>
+            </form>
+        </div >
     )
 }
 
