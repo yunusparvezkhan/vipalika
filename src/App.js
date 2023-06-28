ebab6359a0aa8a060164d6039f32d742f6bd4f6d
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-28 16:37:23 +0530

    style: add better positioning of the entire app contents

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import ItemForm from "./components/ItemForm";
 import ItemSearch from "./components/ItemSearch";
 import ItemList from "./components/ItemList";
 import ItemValue from "./components/ItemValue";
+import './app.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        VipaLika 📃
-      </h1>
-      <ItemForm />
-      {/* <ItemSearch /> */}
-      <ItemList />
-      {/* <ItemValue /> */}
+      <div className="app-container">
+        <h1 className="app-heading">
+          VipaLika 📃
+        </h1>
+        <ItemForm />
+        {/* <ItemSearch /> */}
+        <ItemList />
+        {/* <ItemValue /> */}
+      </div>
     </div>
   );
 }
