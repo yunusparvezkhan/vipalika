082603a5945c1f3a32fc33c42d50e44a08d6e22e
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-28 16:21:14 +0530

    feat: render List mapping to [items] state
    this commit adds logics to ItemsList component to render a list
    according to [items] state of redux store.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const ItemList = () => {
+
+    const items = useSelector((state) => {
+        return state.items;
+    })
+
+    const renderItems = items.map((item, i) => {
+        return (
+            <div>
+                <label><b>{i + 1}. </b></label>
+                <br></br>
+                <label>Name: {item.name}</label>
+                <br></br>
+                <label>Cost: ₹{item.cost}</label>
+                <br></br>
+                <br></br>
+            </div>
+        )
+    })
+
+
     return (
-        <div>List Component</div>
+        <div>
+            {renderItems}
+        </div>
     )
 }
 
