b51f6c9ae93c9f2c07ef403f455db93d823a11e0
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-28 16:27:12 +0530

    style: link bulma in the src/index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
+import 'bulma/css/bulma.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
